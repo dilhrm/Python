55e8eab4161c75a2a18f2f390a9f007f91c28520
dilhrm <dilende2@gmail.com>
2025-10-25 02:01:30 -0400

    continued to work  on it. Finished retrieve.

--- basics/Basics.py
+++ basics/Basics.py
@@ -1,39 +1,79 @@
-import hashlib 
-fname = "passwords.txt"
+fname = "basics/passwords.txt"
 
 text = " "
 running = True 
 webname = ""
 password = ""
+website = ""
 hashed = ""
 choice = ""
+numChoice = 0
 list_websites = []
 list_passwords = []
+
 
 def reset(): 
     text = " "
     webname = ""
     password = ""
+    website = ""
     hashed = ""
     choice = ""
+    numChoice = 0
+
+def readPasswords(): 
+    counter = 0
+    fileIN = open(fname,'r')
+    text = fileIN.readline()
+    while(text != ""): 
+        if(counter%2 == 0): 
+            list_websites.append(text)
+        if(counter%2 == 1): 
+            list_passwords.append(text)
+        text = fileIN.readline()
+        counter += 1
+    fileIN.close()
+    
+def numOf(site, list): 
+    counter = 0
+    for website in list:
+        if(site == website): 
+            return counter
+        counter += 1 
+    return -1
+
 
 while(running):
+    reset()
+    for i in range(3):
+        print()
     choice = input("What do you want to do? \na to add password. \nr to retrieve password. \nl to look at a list of webiste names \nc to change password. \nf to close program.") 
     if(choice == 'f'): 
         running = False 
+
     if(choice == 'a'): 
         fileOUT = open(fname,'a') 
-        webname = input("What is the name of the website?") 
-        password = input("What is the password?") 
-        fileOUT.write(webname)
-        fileOUT.write(password) 
+        webname = input("What is the name of the website? ") 
+        password = input("What is the password? ") 
+        fileOUT.write(webname + "\n")
+        fileOUT.write(password + "\n") 
         fileOUT.close()
 
     if(choice == 'r'): 
-        print(1)
+        readPasswords()
+        website = input("What is the password? ") 
+        numChoice = numOf(website) 
+        if(numChoice == -1): 
+            print("Website does not exist. Ensure website is spelled properly and/or exists.")
+        else: 
+            print(list_passwords[numChoice])
         
     if(choice == 'c'): 
-        print(1)
+        readPasswords() 
+        website = input("What is the website? ") 
+        numChoice = numOf(password) 
+        password = input("New password? ")
+        
         
     if(choice == 'l'): 
         fileIN = open(fname,'r') 
@@ -43,5 +83,3 @@ while(running):
             text = fileIN.readline() 
             text = fileIN.readline() 
         fileIN.close() 
-
-    reset()
